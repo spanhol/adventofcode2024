d98595f438c2b3828ccb7a9ea179bee937b6c7f1
fernando spanhol <ferspanhol@gmail.com>
2024-12-01 01:26:34 -0500

    day one https://adventofcode.com/2024/day/1

---
+++ src/main/java/dev/spanhol/Main.java
@@ -0,0 +1,11 @@
+package dev.spanhol;
+
+import dev.spanhol.one.One;
+
+public class Main {
+    public static void main(String[] args) {
+        One one = new One();
+        one.part1();
+        one.part2();
+    }
+}
